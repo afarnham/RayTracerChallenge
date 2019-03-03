a78cbcd16043c4a411edb00fdb6203d55f80f87d
Aaron Farnham <aaron@foreflight.com>
2019-03-03 16:11:50 -0600

    Initial Commit

---
+++ RayTracerChallenge/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RayTracerChallenge
+//
+//  Created by Raymond Farnham on 3/3/19.
+//  Copyright © 2019 Raymond Farnham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
